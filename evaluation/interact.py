426819c0eabbadd97b374f393770b674dd6d40c1
Jesse Dingley <jesse.dingley@eisti.fr>
2022-02-23 20:30:32 +0100

    updates

--- evaluation/interact.py
+++ evaluation/interact.py
@@ -1,7 +1,9 @@
+from readline import replace_history_item
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch 
 
 MODEL = "DingleyMaillotUrgell/homer-bot"
+# MODEL = "microsoft/DialoGPT-small"
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 model = AutoModelForCausalLM.from_pretrained(MODEL)
@@ -18,14 +20,16 @@ for step in range(5):
     # generated a response while limiting the total chat history to 1000 tokens, 
     chat_history_ids = model.generate(
         bot_input_ids, 
-        max_length=500,                    
+        max_length=1000,                    
         pad_token_id=tokenizer.eos_token_id,  
-        no_repeat_ngram_size=3,
-        # num_beams = 50       
+        # no_repeat_ngram_size=3,
+        # # num_beams = 50       
         do_sample=True, 
-        top_k=200, 
+        top_k=100, 
         top_p=0.7,
+        # length_penalty = 0.5,
         temperature = 0.8
+        # repetition_penalty=1.3 
     )
     
     # pretty print last ouput tokens from bot
