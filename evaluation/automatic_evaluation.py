99c8a8814d9bfbc9c13ab5b203bf9ca516ad46d7
Baptiste Urgell <urgellbapt@eisti.eu>
2022-03-24 11:22:37 +0100

    add cosine similarity and jaccard similarity

--- evaluation/automatic_evaluation.py
+++ evaluation/automatic_evaluation.py
@@ -5,6 +5,10 @@ Calculates:
         (Determines quality of predicted replies compared to groud truth replies)
     - Entropy: 
         (Determines lexical diversity of the bot's generated replies)
+    - Cosine similarity:
+        (Determines proximity beetwen the truth and predicted replies)
+    - Jaccard similarity:
+        (Determines lexical similarity beetwen the truth and predicted replies)
 """
 
 
@@ -14,6 +18,7 @@ Calculates:
 print("Importing libraries...")
 
 # for loading model and tokenizer
+from crypt import METHOD_BLOWFISH
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # for calculating BLEU scores
@@ -22,14 +27,24 @@ from nltk.translate.bleu_score import sentence_bleu
 # for calculating entropy
 import math
 
+# for calculating cosine similarity
+from transformers import BertTokenizer, BertModel
+from torch.nn import CosineSimilarity
 
+# for calculating Jaccard similarity
+from nltk.stem import WordNetLemmatizer
 
+# for dialogue
+import pandas as pd
+from typing import List, Tuple
 
 
 """Constants."""
 
-# model name
+# models names
 MODEL = "DingleyMaillotUrgell/homer-bot"
+BERT = "bert-base-uncased"
+
 
 # number of turns to run the evaluation on
 # (a turn is a turn from user then bot )
@@ -39,13 +54,16 @@ TURNS = 5
 
 
 
-"""Load model and tokenizer."""
-print("Loading model and tokenizer...")
+"""Load model, tokenizer and lemmatizer."""
+print("Loading model, tokenizer and lemmatizer...")
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 model = AutoModelForCausalLM.from_pretrained(MODEL)
 
+tokenizer_bert = BertTokenizer.from_pretrained(BERT)
+model_bert = BertModel.from_pretrained(BERT)
 
+lemmatizer = WordNetLemmatizer()
 
 
 """Metric functions."""
@@ -96,13 +114,98 @@ def entropy(string):
     # calculate the entropy
     return - sum([ p * math.log(p) / math.log(2.0) for p in prob ])
 
+def doc2vect(response_string: str):
+    """Convert document to a vector
 
+    Args:
+        response_string (str): document
+
+    Returns:
+        _type_: document convert into tensor
+    """
+    inputs = tokenizer_bert(response_string, return_tensors="pt")
+    outputs = model_bert(**inputs)
+    return outputs.last_hidden_state[:,0,:]
+
+def cosine_similarity(ref: str, hyp: str)->float:
+    """Calculates cosine similarity
+
+    Args:
+        ref (str): reference string.
+        hyp (str): hypothesis string.
+
+    Returns:
+        float: cosine simalrity
+    """
+    # convert documents to vectors
+    ref_embedding = doc2vect(ref)
+    hyp_embedding = doc2vect(hyp)
+    
+    # calcul cosine similarity beetwen the vectors
+    cos = CosineSimilarity(dim=1, eps=1e-6)
+    output = cos(ref_embedding, hyp_embedding)
+    return output[0]
+
+def doc2words(response_string: str)->List[str]:
+    """Convert a string to a list of words lemmatized.
+
+    Args:
+        response_string (str): document.
+
+    Returns:
+        List[str]: document lemmatized and tokenized.
+    """
+    response_string = response_string.lower()
+    response_token = tokenizer_bert.tokenize(response_string)
+    output = [lemmatizer.lemmatize(word) for word in response_token]
+    return output
+
+def jaccard_similarity(ref: str, hyp: str)->float:
+    """Calculates Jaccard similarity
+
+    Args:
+        ref (str): reference string.
+        hyp (str): hypothesis string.
+
+    Returns:
+        float: Jaccard simalrity
+    """
+    # List the unique words in a document
+    words_ref = set(doc2words(ref))
+    words_hyp = set(doc2words(hyp))
+    
+    # Find the intersection of words list of ref & hyp
+    intersection = words_ref.intersection(words_hyp)
+
+    # Find the union of words list of ref & hyp
+    union = words_ref.union(words_hyp)
+        
+    # Calculate Jaccard similarity score 
+    # using length of intersection set divided by length of union set
+    return float(len(intersection)) / len(union)
+
+"""Parse arguments."""
+from argparse import ArgumentParser
+if __name__ == '__main__':
+    # Parse arguments
+    parser = ArgumentParser()
+    parser.add_argument("--test", type=str, required=True, dest="data_path", help = "data file for testing")
+    parser.add_argument("--generation_methods", type=str, default='all', choices=['all','topk','greedy','beam'], dest="generation_methods", help="The name of the generation method")
+    parser.add_argument("--metrics", type=str, default='all', choices=['all','bleu-2','bleu-4','entropy','ideal entropy','cosine similarity', 'jaccard similarity'], dest="metrics", help="The name of the metrics")
+    args = parser.parse_args()
+    
+DATA_PATH = args.data_path
+METHODS = args.generation_methods
+METRICS = args.metrics
 
 
 """Run dialogue and calculate metrics."""
 print("Running dialogue...")
 
-dialogue = [("Hi Homer!", "Hello!"), ("How are you?", "Good thanks."), ("Do you want a beer?", "Yes please.")]
+dialogue_df: pd.DataFrame = pd.read_csv(filepath_or_buffer=DATA_PATH, header=0)
+dialogue: List[Tuple[str,str]]  = [tuple(x) for x in dialogue_df.values.tolist()]
+
+#dialogue = [("Hi Homer!", "Hello!"), ("How are you?", "Good thanks."), ("Do you want a beer?", "Yes please.")]
 #         #[(other,       homer),    (other,          homer),          (other,                 homer        )]
 
 # limit dialogue to a certain number of turns
@@ -183,35 +286,47 @@ def run_model(generation_method):
     return generated_responses_string, ground_truth_responses_string
 
 
-TOPK_generated_responses_string, TOPK_ground_truth_responses_string = run_model("topk")
-BEAM_generated_responses_string, BEAM_ground_truth_responses_string = run_model("beam")
-GREEDY_generated_responses_string, GREEDY_ground_truth_responses_string = run_model("greedy")
 
-"""Calculate BLEU + entropy."""
-print("TOPK")
-print()
-print(f"BLEU-2: {BLEU(ref = TOPK_ground_truth_responses_string, hyp = TOPK_generated_responses_string, order=2)}")
-print(f"BLEU-4: {BLEU(ref = TOPK_ground_truth_responses_string, hyp = TOPK_generated_responses_string, order=4)}")
-print(f"Entropy: {entropy(TOPK_generated_responses_string)}")
-print(f"Ideal entropy: {entropy_ideal(len(TOPK_generated_responses_string))}")
-print()
-print()
 
-print("BEAM")
-print()
-print(f"BLEU-2: {BLEU(ref = BEAM_ground_truth_responses_string, hyp = BEAM_generated_responses_string, order=2)}")
-print(f"BLEU-4: {BLEU(ref = BEAM_ground_truth_responses_string, hyp = BEAM_generated_responses_string, order=4)}")
-print(f"Entropy: {entropy(BEAM_generated_responses_string)}")
-print(f"Ideal entropy: {entropy_ideal(len(BEAM_generated_responses_string))}")
-print()
-print()
+if METHODS == "all" or METHODS == "topk": 
+    TOPK_generated_responses_string, TOPK_ground_truth_responses_string = run_model("topk")
+    """Calculate BLEU + entropy."""
+    print()
+    print("TOPK")
+    print()
+    if METRICS == "all" or METRICS == "bleu-2": print(f"BLEU-2: {BLEU(ref = TOPK_ground_truth_responses_string, hyp = TOPK_generated_responses_string, order=2)}")
+    if METRICS == "all" or METRICS == "bleu-4": print(f"BLEU-4: {BLEU(ref = TOPK_ground_truth_responses_string, hyp = TOPK_generated_responses_string, order=4)}")
+    if METRICS == "all" or METRICS == "entropy": print(f"Entropy: {entropy(TOPK_generated_responses_string)}")
+    if METRICS == "all" or METRICS == "ideal entropy": print(f"Ideal entropy: {entropy_ideal(len(TOPK_generated_responses_string))}")
+    if METRICS == "all" or METRICS == "cosine similarity": print(f"Cosine similarity: {cosine_similarity(ref = TOPK_ground_truth_responses_string, hyp = TOPK_generated_responses_string)}")
+    if METRICS == "all" or METRICS == "jaccard similarity": print(f"Jaccard similarity: {jaccard_similarity(ref = TOPK_ground_truth_responses_string, hyp = TOPK_generated_responses_string)}")
+    print()
+    print()
+    
+if METHODS == "all" or METHODS == "beam": 
+    BEAM_generated_responses_string, BEAM_ground_truth_responses_string = run_model("beam")
+    print()
+    print("BEAM")
+    print()
+    if METRICS == "all" or METRICS == "bleu-2": print(f"BLEU-2: {BLEU(ref = BEAM_ground_truth_responses_string, hyp = BEAM_generated_responses_string, order=2)}")
+    if METRICS == "all" or METRICS == "bleu-4": print(f"BLEU-4: {BLEU(ref = BEAM_ground_truth_responses_string, hyp = BEAM_generated_responses_string, order=4)}")
+    if METRICS == "all" or METRICS == "entropy": print(f"Entropy: {entropy(BEAM_generated_responses_string)}")
+    if METRICS == "all" or METRICS == "ideal entropy": print(f"Ideal entropy: {entropy_ideal(len(BEAM_generated_responses_string))}")
+    if METRICS == "all" or METRICS == "cosine similarity": print(f"Cosine similarity: {cosine_similarity(ref = BEAM_ground_truth_responses_string, hyp = BEAM_generated_responses_string)}")
+    if METRICS == "all" or METRICS == "jaccard similarity": print(f"Jaccard similarity: {jaccard_similarity(ref = BEAM_ground_truth_responses_string, hyp = BEAM_generated_responses_string)}")
+    print()
+    print()
 
-print("GREEDY")
-print()
-print(f"BLEU-2: {BLEU(ref = GREEDY_ground_truth_responses_string, hyp = GREEDY_generated_responses_string, order=2)}")
-print(f"BLEU-4: {BLEU(ref = GREEDY_ground_truth_responses_string, hyp = GREEDY_generated_responses_string, order=4)}")
-print(f"Entropy: {entropy(GREEDY_generated_responses_string)}")
-print(f"Ideal entropy: {entropy_ideal(len(GREEDY_generated_responses_string))}")
-print()
-print()
-# print("Dialo-GPT entropy: ")
+if METHODS == "all" or METHODS == "greedy": 
+    GREEDY_generated_responses_string, GREEDY_ground_truth_responses_string = run_model("greedy")
+    print()
+    print("GREEDY")
+    print()
+    if METRICS == "all" or METRICS == "bleu-2": print(f"BLEU-2: {BLEU(ref = GREEDY_ground_truth_responses_string, hyp = GREEDY_generated_responses_string, order=2)}")
+    if METRICS == "all" or METRICS == "bleu-4": print(f"BLEU-4: {BLEU(ref = GREEDY_ground_truth_responses_string, hyp = GREEDY_generated_responses_string, order=4)}")
+    if METRICS == "all" or METRICS == "entropy": print(f"Entropy: {entropy(GREEDY_generated_responses_string)}")
+    if METRICS == "all" or METRICS == "ideal entropy": print(f"Ideal entropy: {entropy_ideal(len(GREEDY_generated_responses_string))}")
+    if METRICS == "all" or METRICS == "cosine similarity": print(f"Cosine similarity: {cosine_similarity(ref = GREEDY_ground_truth_responses_string, hyp = GREEDY_generated_responses_string)}")
+    if METRICS == "all" or METRICS == "jaccard similarity": print(f"Jaccard similarity: {jaccard_similarity(ref = GREEDY_ground_truth_responses_string, hyp = GREEDY_generated_responses_string)}")
+    print()
+    print()
